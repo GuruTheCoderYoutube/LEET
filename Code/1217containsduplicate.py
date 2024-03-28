bb0bb811ee3ce3909792accf1e5b9e042a647ca7
GuruTheCoderYoutube <gururajcoder@gmail.com>
2024-03-28 07:12:58 +0530

    first commit

---
+++ Code/1217containsduplicate.py
@@ -0,0 +1,9 @@
+def containsduplicate(array):
+    hashset = set()
+    for num in array:
+        if num in hashset:
+            return True
+        hashset.add(num)
+    return False
+
+#hashmap/hashtable, array
