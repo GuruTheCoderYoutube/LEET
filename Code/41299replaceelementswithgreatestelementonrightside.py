791d454cfc8ef4b09800b60880200cabf6c239ef
GuruTheCoderYoutube <gururajcoder@gmail.com>
2024-03-30 15:56:05 +0530

    sdf

--- Code/41299replaceelementswithgreatestelementonrightside.py
+++ Code/41299replaceelementswithgreatestelementonrightside.py
@@ -1,9 +1,9 @@
-def replaceElements(arr):
+def replaceElement(arr):
     rightMax = -1
     for i in range(len(arr)-1, -1, -1):
-        newMax = max(rightMax, arr[i])
+        newMax = max(arr[i], rightMax)
         arr[i] = rightMax
         rightMax = newMax
-    return arr
+
 
 # Array manipulation, infoTrackingLoop, unusualTraversal
